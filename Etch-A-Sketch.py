1e961057c437bcf25c4807f4b1ac9ecc95873304
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2024-06-10 23:08:22 +0530

    Update Etch-A-Sketch.py

--- Etch-A-Sketch.py
+++ Etch-A-Sketch.py
@@ -38,7 +38,7 @@ screen.listen()  # listening events
 screen.onkey(key="w", fun=move_forward)  # onkey() is an event listener. It calls move_forward when space is pressed
 # onkey() is a higher order function that takes another function as input.
 # No () at the end of move_forward when passed as input because we want move_forward to execute when space is pressed.
-screen.onkey(key="s", fun=move_backward)  # use positional arguments for higher order functions
+screen.onkey(key="s", fun=move_backward)  # use keyword arguments for built-in methods
 screen.onkey(key="a", fun=clock_wise)
 screen.onkey(key="d", fun=anti_clock)
 screen.onkey(key="c", fun=clear)

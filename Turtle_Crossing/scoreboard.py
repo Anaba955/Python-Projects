eec897bee3dcdf83f35c1a124d81ac548943d480
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2024-06-15 23:19:01 +0530

    Update scoreboard.py

--- Turtle_Crossing/scoreboard.py
+++ Turtle_Crossing/scoreboard.py
@@ -1,6 +1,5 @@
 from turtle import Turtle
 FONT = ("Courier", 24, "bold")
-ALIGNMENT = "center"
 
 
 class Scoreboard(Turtle):
@@ -10,12 +9,12 @@ class Scoreboard(Turtle):
         self.level = 1
         self.hideturtle()
         self.penup()
-        self.goto(-210, 250)
+        self.goto(-280, 250)
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
-        self.write(arg=f"Level: {self.level}", align=ALIGNMENT, font=FONT)
+        self.write(arg=f"Level: {self.level}", align="left", font=FONT)
 
     def level_up(self):
         self.level += 1
@@ -23,4 +22,4 @@ class Scoreboard(Turtle):
 
     def game_over(self):
         self.goto(0, 0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
+        self.write(arg="GAME OVER", align="center", font=FONT)

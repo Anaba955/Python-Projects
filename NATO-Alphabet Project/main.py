5c816a8b084898482ecd7026312140947413c6b8
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2024-07-06 17:07:37 +0530

    Update main.py

--- NATO-Alphabet Project/main.py
+++ NATO-Alphabet Project/main.py
@@ -4,6 +4,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-user_input = input("Enter a word: ").upper()
-letter_list = [phonetic_dict[letter] for letter in user_input]
-print(letter_list)
+
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        letter_list = [phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, letters in alphabet please.")
+        generate_phonetic()
+    else:
+        print(letter_list)
+
+
+generate_phonetic()
